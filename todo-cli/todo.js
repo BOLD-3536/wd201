aa9e08e56c5f06be620efb62abd0a2bc82746885
BOLD-3536 <126971449+BOLD-3536@users.noreply.github.com>
2023-04-12 15:35:07 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,37 +1,70 @@
-const todoList = () => {
-  all = [];
+function todoList() {
+   all = [];
+   date =new  Date()
   const add = (todoItem) => {
-    all.push(todoItem);
-  };
+    all.push(todoItem)
+  }
   const markAsComplete = (index) => {
-    all[index].completed = true;
-  };
+    all[index].completed = true
+  }
 
-  const overdue = () => {
-    return all.filter(
-      (todo) => todo.dueDate < new Date().toLocaleDateString("en-CA")
-    );
-  };
+    const overdue = () => {
+        return all.filter(
+            (i) => i.dueDate.split("-")[2] < date.getDate()
+        );
+    // Write the date check condition here and return the array
+    // of overdue items accordingly.
+      
+  }
 
   const dueToday = () => {
-    return all.filter(
-      (todo) => todo.dueDate === new Date().toLocaleDateString("en-CA")
-    );
-  };
+    // Write the date check condition here and return the array
+    // of todo items that are due today accordingly.
+      return all.filter(
+          (i) => i.dueDate.split("-")[2] == date.getDate()
+      );
+  }
 
   const dueLater = () => {
-    return all.filter(
-      (todo) => todo.dueDate > new Date().toLocaleDateString("en-CA")
-    );
-  };
+    // Write the date check condition here and return the array
+    // of todo items that are due later accordingly.
+      return all.filter(
+          (i) => i.dueDate.split("-")[2] > date.getDate()
+      );
+  }
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string
+    // as per the format given above.
+      return list.map((i) => {
+          const wt = i.title.trim();
+          if (i.completed == true) {
+              t = "[x]";
+          }
+          else {
+              t = "[]";
+          }
+          const p =
+              i.dueDate.split("-")[2] == date.getDate()
+                  ? ""
+                  : i.dueDate;
+          let k = "";
+          k = t + " " + wt + " " + p;
+          return k;
+    
+      })
+      .join("\n")
+  }
+
   return {
     all,
     add,
-    markAsComplete,
+    markAsComplete
     overdue,
     dueToday,
     dueLater,
-  };
-};
+    toDisplayableList
+  }
+}
 
-module.exports = todoList;
+module.export = todoList;
